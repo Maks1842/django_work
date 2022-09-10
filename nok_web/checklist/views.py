1396a320558ee6b2633515f28a2f3fe3daf6e790
Maks1842 <maks1842@yandex.ru>
2022-09-10 13:17:44 +0300

    Удалил ненужные импорты из views.py

--- nok_web/checklist/views.py
+++ nok_web/checklist/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
 
 from .models import *
 from .forms import UserRegisterForm, UserLoginForm
-# from .utils import MyMixin
 from django.contrib import messages
 from django.contrib.auth import login, logout
 
